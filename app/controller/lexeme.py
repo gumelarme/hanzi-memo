e49a39e742b1f72e486e5d52e577e1dd49420b15
gumelarme <gumelar.pn@gmail.com>
2023-11-26 00:06:18 +0800

    Feat: example getter

--- app/controller/lexeme.py
+++ app/controller/lexeme.py
@@ -5,7 +5,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.controller.base import D, d
-from app.db.model import Dictionary, Lexeme
+from app.db.model import Example, Lexeme, lexeme_example
 
 
 class LexemeDTO(SQLAlchemyDTO[Lexeme]):
@@ -20,3 +20,16 @@ class LexemeController(Controller):
     async def get_lexeme(self, tx: AsyncSession, lex_id: str) -> D[Lexeme]:
         query = select(Lexeme).where(Lexeme.id == lex_id)
         return d(await tx.scalar(query))
+
+    # XXX: Untested with real data
+    @get("/{lex_id:str}/examples", return_dto=SQLAlchemyDTO[Example])
+    async def get_lexeme_example(
+        self,
+        tx: AsyncSession,
+        lex_id: str,
+    ) -> D[list[Example]]:
+        query = (
+            select(Example).join(lexeme_example).join(Lexeme).where(Lexeme.id == lex_id)
+        )
+
+        return d(await tx.scalars(query))
