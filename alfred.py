bc42be083de290767d684d4c5208cb868c7b45ef
gumelarme <gumelar.pn@gmail.com>
2023-11-28 21:54:54 +0800

    Fix: asyncio.run in alfred

--- alfred.py
+++ alfred.py
@@ -8,22 +8,22 @@ from app.db.connection import get_engine
 from app.db.seed import migrate_schema, seed_collection, seed_dict, seed_text
 
 
-def migrate(args: list[str]):
+async def migrate(args: list[str]):
     drop = len(args) == 1 and args[0] == "drop"
-    asyncio.run(migrate_schema(get_engine(), drop))
+    await migrate_schema(get_engine(), drop)
 
 
 commands: dict[str, Callable] = {
     "migrate": migrate,
-    "seed_dict": lambda args: asyncio.run(seed_dict(*args)),
-    "seed_coll": lambda args: asyncio.run(seed_collection(args)),
-    "seed_text": lambda args: asyncio.run(seed_text(args)),
+    "seed_dict": seed_dict,
+    "seed_coll": seed_collection,
+    "seed_text": seed_text,
 }
 
 seeding_func = Callable[[str, int, int | None], None]
 
 
-def main():
+async def main():
     cmd, *args = sys.argv[1:]
     if cmd not in commands:
         print(f"Command {cmd!r} not found")
@@ -34,11 +34,17 @@ def main():
         source, *start_end = args
         start = int(start_end[0]) if len(start_end) > 0 else 0
         end = int(start_end[1]) if len(start_end) > 1 else None
-        commands[cmd]([source, start, end])
+        await commands[cmd](source, start, end)
     else:
-        commands[cmd](args)
+        await commands[cmd](args)
 
 
 if __name__ == "__main__":
     load_dotenv()
-    main()
+
+    asyncio.set_event_loop(asyncio.SelectorEventLoop())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+
+    if loop.is_running():
+        loop.close()
