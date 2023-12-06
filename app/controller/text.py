6c623cae30bfaf59ed2c0cce812b52baf64de341
gumelarme <gumelar.pn@gmail.com>
2023-12-06 23:24:07 +0800

    Feat: /texts end point did not include preset text

--- app/controller/text.py
+++ app/controller/text.py
@@ -4,7 +4,9 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.controller.base import D, d
-from app.db.model import Text
+from app.db.model import Preset, Text
+
+exclude_presets = select(Preset.text_id)
 
 
 class TextController(Controller):
@@ -13,10 +15,10 @@ class TextController(Controller):
 
     @get("/")
     async def get_texts(self, tx: AsyncSession) -> D[list[Text]]:
-        query = select(Text).limit(100)
+        query = select(Text).where(Text.id.not_in(exclude_presets)).limit(100)
         return d((await tx.scalars(query)).all())
 
     @get("/{text_id:str}")
     async def get_one_text(self, tx: AsyncSession, text_id: str) -> D[Text]:
-        query = select(Text).where(Text.id == text_id)
+        query = select(Text).where(Text.id == text_id, Text.id.not_in(exclude_presets))
         return d((await tx.scalars(query)).one())
