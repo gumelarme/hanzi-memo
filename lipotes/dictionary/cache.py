593c4f3b960d31df94319385d7e28446b72dfcf2
gumelarme <gumelar.pn@gmail.com>
2024-04-11 13:24:06 +0800

    fix: use predifined prefix for lexeme keys

--- lipotes/dictionary/cache.py
+++ lipotes/dictionary/cache.py
@@ -7,10 +7,17 @@ class LexemeCache:
         self.KEY_LEXEME = "lexeme"
         self.KEY_UNAVAILABLE_LEXEME = "unavailable_lexeme"
 
+    @classmethod
+    def zh_prefix(cls, is_simplified=True) -> str:
+        return "zh_sc" if is_simplified else "zh_tc"
+
     def cache_lexemes(
-        self, char: str, lexemes: list[dict[str, str]], prefix="zh_sc"
+        self,
+        char: str,
+        lexemes: list[dict[str, str]],
+        is_sc=True,
     ) -> None:
-        sc_key = f"{prefix}:{char}"
+        sc_key = f"{self.zh_prefix(is_sc)}:{char}"
         if not lexemes:
             self.r.sadd(sc_key, 0)
             return
@@ -23,17 +30,17 @@ class LexemeCache:
                 mapping={k: v for k, v in lex.items() if k != "id"},
             )
 
-    def get_lexemes(self, char: str, prefix="zh_sc") -> list[dict]:
+    def get_lexemes(self, char: str, is_sc=True) -> list[dict]:
         result = []
-        for member in self.r.smembers(f"{prefix}:{char}"):
+        for member in self.r.smembers(f"{self.zh_prefix(is_sc)}:{char}"):
             data = self.r.hgetall(f"{self.KEY_LEXEME}:{member}")
             data["id"] = member
             result.append(data)
         return result
 
-    def is_lexeme_unavailable(self, char: str, prefix="zh_sc") -> bool:
-        key = f"{self.KEY_UNAVAILABLE_LEXEME}:{prefix}"
+    def is_lexeme_unavailable(self, char: str, is_sc=True) -> bool:
+        key = f"{self.KEY_UNAVAILABLE_LEXEME}:{self.zh_prefix(is_sc)}"
         return bool(self.r.sismember(key, char))
 
-    def set_lexeme_unavailable(self, char: str, prefix="zh_sc") -> None:
-        self.r.sadd(f"{self.KEY_UNAVAILABLE_LEXEME}:{prefix}", char)
+    def set_lexeme_unavailable(self, char: str, is_sc=True) -> None:
+        self.r.sadd(f"{self.KEY_UNAVAILABLE_LEXEME}:{self.zh_prefix(is_sc)}", char)
